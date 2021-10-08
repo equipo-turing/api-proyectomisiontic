652e76afd9031d0a3b5149bc0560aea6f63b8fa5
Rojas-Andres <andresfelipe200004@gmail.com>
2021-10-08 17:29:25 -0500

    Creando api para crear un nuevo vendedor

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-import Express from 'express';
+// hacer el import de express tradicional
+// const express = require('express');
 
+// hacer el nuevo import
+import Express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import { conectarBD } from './db/db.js';
+import rutasVendedor from './views/vendedores/rutas.js';
+
+dotenv.config({ path: './.env' });
 
 const app = Express();
+
 app.use(Express.json());
+app.use(Cors());
+app.use(rutasVendedor);
 
+const main = () => {
+  return app.listen(process.env.PORT, () => {
+    console.log(`escuchando puerto ${process.env.PORT}`);
+  });
+};
 
-app.listen(5000,()=>{
-    console.log("Server iniciado papa")
-});
+conectarBD(main);
