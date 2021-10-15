3c885263a446cceda87e5aca5ff150698825ee88
Rojas-Andres <andresfelipe200004@gmail.com>
2021-10-14 19:00:24 -0500

    Añadiendo seguridad

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import Express from 'express';
 import Cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 import rutasVendedor from './views/vendedores/rutas.js';
 import rutasUsuario from './views/usuarios/rutas.js';
 import rutasProducto from './views/producto/rutas.js';
@@ -16,6 +18,19 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://turing-misiontic.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-turing-mintic',
+issuer: 'https://turing-misiontic.us.auth0.com/',
+algorithms: ['RS256']
+});
+app.use(jwtCheck);
 app.use(rutasVendedor);
 app.use(rutasUsuario);
 app.use(rutasProducto);
